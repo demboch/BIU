1eaa30ecc4ebd9f451fe0817dbc726386c6bb4e6
Maciej Dembowski <dembochdc@gmail.com>
2016-11-29 00:50:40 +0100

    Poprawki

--- ZAD2/scripts/PeopleTableViewModel.js
+++ ZAD2/scripts/PeopleTableViewModel.js
@@ -1,18 +1,18 @@
-function PeopleTableViewModel(config) {
+function PeopleTableViewModel(config) { // konstruktor
     var self = this;
     self.people = new ListOfPeople();
     self.currentPage = 0;
     self.pageSize = config.pageSize;
     self.context = config.context;
-    
-    var getData = function (begin, end) {
+
+    var getData = function(begin, end) {
         if (end > data.length) {
             end = data.length;
         }
         if (begin < 0) {
             begin = 0;
         }
-        for (var i = begin; i<end; i+=1) {
+        for (var i = begin; i < end; i += 1) {
             self.people.addPerson(data[i]);
         }
     }
@@ -26,7 +26,7 @@ function PeopleTableViewModel(config) {
         self.currentPage++;
         self.context.innerHTML = self.people.toTable();
     };
-    
+
     self.change = function(pgSize) {
         //alert('zmień ilość');
         self.pageSize = pgSize;
@@ -37,7 +37,7 @@ function PeopleTableViewModel(config) {
     self.prev = function() {
         //alert('kliknąłęś poprzedni');
         self.people.clear();
-        if (self.currentPage-1 >= 0) {
+        if (self.currentPage - 1 >= 0) {
             self.currentPage--;
         }
         var begin = (self.currentPage) * self.pageSize;
@@ -45,11 +45,11 @@ function PeopleTableViewModel(config) {
         getData(begin, end);
         self.context.innerHTML = self.people.toTable();
     };
-    
+
     self.sort = function(comparer) {
+        comparator.setState();
         data.sort(comparer);
         self.currentPage = 0;
         self.next();
     };
 }
-
--- ZAD2/scripts/scripts.js
+++ ZAD2/scripts/scripts.js
@@ -7,35 +7,14 @@ var viewModel = new PeopleTableViewModel({
 var comparator = new Comparators();
 
 function init() {
-//    var listOfPeople = new ListOfPeople();
-//    
-//    for (var i = 0; i < data.length; i++ ) {
-//            listOfPeople.addPerson(data[i]);
-//        }
-//    
-//    var context = document.getElementById('table');
-//    context.innerHTML = listOfPeople.toTable();
-    
+    //    var listOfPeople = new ListOfPeople();
+    //    
+    //    for (var i = 0; i < data.length; i++ ) {
+    //            listOfPeople.addPerson(data[i]);
+    //        }
+    //    
+    //    var context = document.getElementById('table');
+    //    context.innerHTML = listOfPeople.toTable();
+
     viewModel.next();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

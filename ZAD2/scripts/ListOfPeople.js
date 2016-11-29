af06572b045a794868678623274e8ada314063db
Maciej Dembowski <dembochdc@gmail.com>
2016-11-29 13:22:51 +0100

    Add Hide Buttons

--- ZAD2/scripts/ListOfPeople.js
+++ ZAD2/scripts/ListOfPeople.js
@@ -6,22 +6,28 @@ function ListOfPeople() {
     };
 
     var generateTableHeader = function() {
-        return '<tr><th><button>LP</button></th> <th><button onclick="viewModel.sort(comparator.byId)">ID</button></th> <th id="column"><button onclick="viewModel.sort(comparator.byName)">Name</button></th> <th><button onclick="viewModel.sort(comparator.bySurname)">Surname</button></th>  <th><button onclick="viewModel.sort(comparator.byGender)">Gender</button></th> <th><button onclick="viewModel.sort(comparator.byAge)">Age</button></th> <th><button onclick="viewModel.sort(comparator.byEmail)">Email</button></th>  <th><button onclick="viewModel.sort(comparator.byIncome)">Income</button></th>  <th><button onclick="viewModel.sort(comparator.byBirthsday)">Birthsday</button></th></tr>';
+        return '<tr><th id="column-a"><button>LP</button></th> <th id="column-b"><button onclick="viewModel.sort(comparator.byId)">ID</button></th> <th id="column-c"><button onclick="viewModel.sort(comparator.byName)">Name</button></th> <th id="column-d"><button onclick="viewModel.sort(comparator.bySurname)">Surname</button></th>  <th id="column-e"><button onclick="viewModel.sort(comparator.byGender)">Gender</button></th> <th id="column-f"><button onclick="viewModel.sort(comparator.byAge)">Age</button></th> <th id="column-g"><button onclick="viewModel.sort(comparator.byEmail)">Email</button></th>  <th id="column-h"><button onclick="viewModel.sort(comparator.byIncome)">Income</button></th> <th id="column-i"><button onclick="viewModel.sort(comparator.byBirthsday)">Birthsday</button></th></tr>';
     }
 
     var hide = function() {
-        return '<th class="show-hide">' +
-            // '<select id="select-hide-td">' +
-            // '<option value="gender">Gender</option>' +
-            // '<option value="firstName">Name</option>' +
-            // '<option value="lastName">Surname</option>' +
-            // '<option value="email">Email</option>' +
-            // '<option value="income">Income</option>' +
-            // '<option value="birthsday">Birthsday</option>' +
-            // '<option value="age">Age</option>' +
-            // '</select>' +
-            '<button type="button" data-column="#column" id="select-show-td">Hide</button>' +
-            '</th>';
+        return '<button type="button" data-column="#column-a" id="select-show-td">Hide LP</button>' +
+            '<button type="button" data-column="#column-b" id="select-show-td">Hide ID</button>' +
+            '<button type="button" data-column="#column-c" id="select-show-td">Hide Name</button>' +
+            '<button type="button" data-column="#column-d" id="select-show-td">Hide Surname</button>' +
+            '<button type="button" data-column="#column-e" id="select-show-td">Hide Gender</button>' +
+            '<button type="button" data-column="#column-f" id="select-show-td">Hide Age</button>' +
+            '<button type="button" data-column="#column-g" id="select-show-td">Hide Email</button>' +
+            '<button type="button" data-column="#column-h" id="select-show-td">Hide Income</button>' +
+            '<button type="button" data-column="#column-i" id="select-show-td">Hide Birthsday</button>';
+        // '<select id="select-hide-td">' +
+        // '<option value="gender">Gender</option>' +
+        // '<option value="firstName">Name</option>' +
+        // '<option value="lastName">Surname</option>' +
+        // '<option value="email">Email</option>' +
+        // '<option value="income">Income</option>' +
+        // '<option value="birthsday">Birthsday</option>' +
+        // '<option value="age">Age</option>' +
+        // '</select>' +
     }
 
     $(document).on("click", "[data-column]", function() {

1bd8c1d8681ad0fbdaea2317e5c9de267731f0f3
Maciej Dembowski <dembochdc@gmail.com>
2016-12-06 14:39:40 +0100

    init

---
+++ ZAD3/my-dream-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TESTComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TESTComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
